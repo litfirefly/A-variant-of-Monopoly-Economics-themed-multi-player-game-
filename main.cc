2a22f5fd99f6a1580d6366420b290df520f7133a
parikhdev0 <38706129+parikhdev0@users.noreply.github.com>
2020-08-04 23:30:26 -0400

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "board.h"
+using namespace std;
+
+int main(){
+	cout << "Welcome to Watopoly" << endl;	
+	Board game;
+	string load_file = "";
+	bool testing = false;
+	for (int i=0; i < argc; i++){
+		if (argv[i]=="-testing"){
+			testing=true;
+		}
+		else if (argv[i]=="-load"){
+			if ((i+1)<argc){
+				load_file = argv[i+1];
+			}
+		}
+	}
+	if (load_file!=""){
+		cout << "You have loaded the file: " << load_file << endl;
+		game.loadGame(load_file);
+	}
+
+	else{
+		if (testing){
+			cout << "You have started Watopoly in Testing Mode." << endl;
+			game.setTesting();
+		}
+	
+	
+		cout << "How many players will be playing today?" << endl;
+		int count_players = 0;
+		while(true){
+			cin>>count_players;
+			if (count_players<2 || count_players>8){
+				cout << "This is an invalid number of players. Their must be 2-8 players playing." << endl;
+				continue;
+			}
+			break;
+		}
+		game.initialize(count_players);
+		game.playTurn();
+	}
+
+	return 0;
+}
