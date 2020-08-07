bbc130aa203a4f7b3e9e6b5d5b6fc1ceadc56174
parikhdev0 <38706129+parikhdev0@users.noreply.github.com>
2020-08-07 15:29:57 -0400

    Add files via upload

--- textdisplay.cc
+++ textdisplay.cc
@@ -7,38 +7,123 @@
 
 using namespace std;
 
-TextDisplay::TextDisplay(size_t n): n{n} {
-	for (size_t row = 0; row < n; row++){
-		vector<char> row_vec;
-		for (size_t col = 0; col < n; col++){	
-			row_vec.push_back(95);
+    explicit TextDisplay(std::vector<std::vector<Square>> textdisplay);
+    virtual void notify( Subject & whoNotified ) override;
+    virtual void notify() override;
+
+    void printSquare(shared_ptr<Square> square);
+    void print();
+
+TextDisplay::TextDisplay(vector<shared_ptr<Square>> textdisplay): textdisplay{textdisplay} {
+	ifstream board_text("board.txt");
+	string line;
+	int row=0;
+	while (getline(board_text,line)){
+		char c;
+		int col=0;
+		while (file.get(c)){
+			board[row][col]=c;
+			col++;
 		}
-		textdisplay.push_back(row_vec);
+		row++;
 	}
 }
 
+
+
 void TextDisplay::notify( Subject & whoNotified){
-	Info temp = whoNotified.getInfo();
-	if (temp.state==State::Alive){
-		textdisplay[temp.row][temp.col] = 88;
-	}
-	else {
-		textdisplay[temp.row][temp.col] = 95;
-	}
+	//UpdateThisSquare
 }
 
 void TextDisplay::notify(){}
 
-ostream &operator<<(ostream &out, const TextDisplay &td){
-	for (size_t row =0; row < td.n; row++){
-		for (size_t col=0; col < td.n; col++){
-			out << td.textdisplay[row][col];
+void TextDisplay::updateSquare(shared_ptr<Square> square, int rown, int coln){
+		row = (rown+1)*10;
+		col = (coln+1)*10;
+		if (square->owned){
+			board[row-3][col-9]="O";
+			board[row-3][col-8]=":";
+			board[row-3][col-7]=square->owned->getPiece();
 		}
-		if (row!=(td.n-1)){
-			out << endl;
+		else{
+			board[row-3][col-9]=" ";
+			board[row-3][col-8]=" ";
+			board[row-3][col-7]=" ";
 		}
-	}
-	return out;
+
+		if (squares->improvement_level>0){
+			if (improvement_level==1){
+				board[row-3][col-6]="I";
+			}
+			else if (improvement_level==1){
+				board[row-3][col-6]="I";
+				board[row-3][col-5]="I";
+			}
+			else if (improvement_level==1){
+				board[row-3][col-6]="I";
+				board[row-3][col-5]="I";
+				board[row-3][col-4]="I";
+			}
+			else if (improvement_level==4){
+				board[row-3][col-6]="I";
+				board[row-3][col-5]="V";
+			}
+			else if (improvement_level>4){
+				board[row-3][col-6]="V";
+			}
+		}
+		if (squares->mortgaged){
+			board[row-3][col-2] = "M";
+		}
+		int count=0;
+		for (int j=0; i<players.size(); i++){
+			if (players[j]->getPosition()==squares->getPosition()){
+				board[row-2][col-9+count]=players[j]->getPiece();
+				count++;
+			}
+		}
+		
+
 }
+void print(){
+	vector<vector<char>> board;
+	ifstream board_text("board.txt");
+	string line;
+	int row=0;
+	while (getline(board_text,line)){
+		char c;
+		int col=0;
+		while (file.get(c)){
+			board[row][col]=c;
+			col++;
+		}
+		row++;
+	}
+	int sidel = squares.size()/4;
+	
+	//botRows
+	for (int i=0; i<sidel; i++){
+		updateSquare(squares[i], 9, 9-i);	
+	}
+
+	//colRows
+	for (int i=0; i<sidel-2; i++){
+		updateSquare(squares[sidel+i], 8-i; 0);
+		updateSquare(squares[39-i], 8-i; 9);
+	}
+	
+	//TopRow
+	for (int i=0; i<sidel; i++){
+		updateSquare([19+i],0,i);
+	}
+
+	for (int i=0; i<board.size(); i++){
+		for (int j=0; j<board[i].size(); j++){
+			cout<<board[i][j]<<endl;
+		}
+		cout << endl;
+	}
+}
+
 
 
--- textdisplay.h
+++ textdisplay.h
@@ -5,20 +5,21 @@
 #include <iostream>
 #include <vector>
 #include "observer.h"
+#include "square.h"
 
 class Cell;
 
 class TextDisplay: public Observer {
-	    std::vector<std::vector<char>> textdisplay;
-	    size_t n;
+	    std::vector<std::shared_ptr<Square>> squares;
+	    std::vector<std::shared_ptr<Player>> players;
+	    vector<vector<char>> board;
+    	    void updateSquare(int row, int col);
  public:
-    explicit TextDisplay( size_t n );
+    explicit TextDisplay(std::vector<std::vector<Square>> textdisplay);
     virtual void notify( Subject & whoNotified ) override;
     virtual void notify() override;
-
-    friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
+    void print();
 };
 
-std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
 
 #endif
