523b1750959d4ebdaa43431651a529477770b7f2
parikhdev0 <38706129+parikhdev0@users.noreply.github.com>
2020-08-07 00:43:16 -0400

    Add files via upload

--- player.cc
+++ player.cc
@@ -3,6 +3,7 @@
 #include <stdexcept>
 using namespace std;
 Player::Player(char piece, std::string name): piece{piece}, name{name}, position{0}, inJail{false}, jailTurns{-1}, isBankrupt{false}, money{1500}, numOfResidences{0}, numOfGyms{0}{}
+
 void Player::printPlayerAssets(){
 	cout << "Name: " << name << endl;
 	cout << "Piece: " << piece << endl;
@@ -25,12 +26,31 @@ void Player::printPlayerAssets(){
 		cout << squaresOwned[i] << endl;
 	}
 }
+
+
+
+
 void Player::transferMoney(std::shared_ptr<Player> to, int amount){
-	int fail = subtractMoney(amount);
-	if (!fail){
+	try(){
+		subtractMoney(amount);
 		to->addMoney(amount);
 	}
-	return fail;
+	catch(const invalid_argument& ia){
+			cerr << ia.what() << endl;
+	}
+}
+
+int Player::getMoney(){
+	return money;
+}
+
+int Player::getWorth(){
+	int property_worth = 0;
+	for (int i=0; i<squaresOwned; i++){
+		property_worth+=squaresOwned[i].getValue();
+	}	
+	return property_worth+money;
+
 }
 
 void Player::transferProperty(std::shared_ptr<Player> to, std::shared_ptr<Square> square){
@@ -55,24 +75,162 @@ void Player::addSquare(std::shared_ptr<Square> square){
 }
 	
 void Player::addMoney(int amount){
-	if (amount<=0 || isBankrupt){
+	if (amount<=0){
+		cout << "Trying to add an amount less than 0." << endl;
 		return;
 	}	
-	money+=amount;
+	else if (isBankrupt){
+		cout << "Player is bankrupt." << endl;
+	}
+	else{
+		money+=amount;
+		cout << "You completed the transaction. Your current balance is now: " << money << endl;
+	}
+	return;
 }
 
-void Player::subtractMoney(int amount){
-
+void Player::subtractMoney(int amount, vector<shared_ptr<Player>> otherPlayers){
 	if (isBankrupt){
 		throw invalid_argument("Player is bankrupt.");
 		return;
 	}
-	if (money < amount){
-		throw invalid_arguement("Player doesn't have enough money.");
-		return;
+	else if (money >= amount){
+		money-=amount;
+		cout << "You completed the transaction. Your current balance is now: " << money << endl;
 	}
-	money-=amount;
-	return;
+	else {	
+		cout << "Your do not have enough money for this transaction. These are your assets: " << endl;
+		player->printPlayerAssets();
+		cout << "The following commands are available to you: " << endl;
+		cout << "   bankrupt" << endl;
+		cout << "   mortgage <property>" << endl;
+		cout << "   improve <property> sell" << endl;
+		cout << "   trade <player_name> <property> <property>" << endl;
+		cout << "   trade <player_name> <property> <money>" << endl;
+		
+		string line = "";
+		while(money < amount){
+			
+			cout << "Enter a valid command: " << endl;
+			stringstream ss(line);
+			string temp;
+			vector<string> command;
+			while (ss >> temp){
+				stack_of_inputs.push_back(temp);
+			}
 
+			if (command[0]=="bankrupt"){
+				isBankrupt = true;
+				return;
+			}
+			else if (command[0]=="mortgage"){
+				if (command.size()!=1){
+					for (int i=0; i<squaresOwned; i++){
+						if(squaresOwned[i]->getName()==command[1]){
+							squaresOwned[i]->mortgage(this);
+							break;
+						}
+					}
+				}
+			}
+			else if (command[0]== "improve"){
+				if (command.size()!=3 || command[2]!="sell"){
+					"invalid command";
+				}
+				else{
+					for (int i=0; i<squaresOwned; i++){
+						if(squaresOwned[i]->getName()==command[1]){
+							squaresOwned[i]->improveSell(this);
+							break;
+						}
+					}
+				}
+			}
+			else if (command[0]=="trade"){
+				if (command.size()!=4){
+					int index = 0;
+					for (int i=0; i<otherPlayers.size(); i++){
+						if (otherPlayers[index]->getName()==command[1]){
+							int value=-1;
+							if (stoi(command[3],value)){
+								if (otherPlayers[index].getMoney()>=value){
+									cout << "Does " << command[3] << " accept? (Type accept to accept, anything else to reject)" << endl;
+									string accept="";
+									getline(accept);
+									if (accept=="accept"){
+										bool found=false;
+										for (int i=0; i<squaresOwned;i++){
+											if (squaresOwned[i]->getName()==command[2]){
+												transferSquare(otherPlayers[index],squaredOned[i];
+												otherPlayers[index]->subtractMoney(value);
+												found=true;
+												break;
+											}
+										}
+										if (!found){
+											cout << "You don't have the propety" << endl;	
+										}
+									}
+									else{
+										cout << "Trade rejected" << endl;
+									}
+								}
+								else{
+									cout << command[3] << " doesn't have enough money." << endl;
+								}
+							}
+
+							else {
+								bool found=false;
+								int myIndex=-1;
+								for (int i=0; i<squaresOwned;i++){
+									if (squaresOwned[i]->getName()==command[2]){
+										myIndex=i;	
+										found=true;
+										break;
+									}
+								}
+								if (!found){
+									cout << "You don't have the propety" << endl;	
+									break;
+								}
+								else{
+
+									cout << "Does " << command[3] << " accept? (Type accept to accept, anything else to reject)" << endl;
+									string accept="";
+									getline(accept);
+									if (accept=="accept"){
+										bool found=false;
+										for (int i=0; i<otherPlayers[index]->squaresOwned;i++){
+											if (otherPlayers[index]->squaresOwned[i]->getName()==command[2]){
+												transferSquare(this,otherPlayers[index]->squaredOned[i];
+												transferSquare(otherPlayers, squresOwned[i]);
+												found=true;
+												break;
+											}
+										}
+										if (!found){
+											cout << "Other player don't have the propety" << endl;	
+										}
+									}
+									else{
+										cout << "Trade rejected" << endl;
+									}
+								}
+							}
+							break;
+						}
+					}
+				}	
+			}
+		}
+		money-=amount;
+		cout << "You raised enough money, and paid the transaction. Your current balance is now: " << money << endl;
+	}
+	return;
+}
+
+void Player::move(int position){
+	player->position+=position;
 }
 
