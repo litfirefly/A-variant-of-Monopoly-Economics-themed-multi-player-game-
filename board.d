ce8539f9305a41951a769c7048c5c7d402a4fb17
parikhdev0 <38706129+parikhdev0@users.noreply.github.com>
2020-08-13 22:26:35 -0400

    Delete board.d

--- board.d
+++
@@ -1,3 +0,0 @@
-board.o: board.cc board.h square.h player.h academic.h coop.h \
- dcTimsLine.h goosenesting.h goToTims.h gym.h needleshall.h osap.h \
- residences.h slc.h tuition.h textdisplay.h observer.h
